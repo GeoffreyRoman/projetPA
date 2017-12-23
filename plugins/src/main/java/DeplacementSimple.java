3708a764df4bc7e5fd9afc1f2bade5003511fdd8
Gael Fontaine <gael.fontaine@etu.unice.fr>
2017-12-23 19:11:11 +0100

    Test de création de module maven

---
+++ plugins/src/main/java/DeplacementSimple.java
@@ -0,0 +1,34 @@
+package plugin;
+
+import java.util.Random;
+
+import annotation.Deplacement;
+import moteur.Robot;
+
+public class DeplacementSimple {
+
+	@Deplacement
+	public void deplacement(Robot r) {
+		int arenaXMax = 450;
+		int arenaYMax = 450;
+		Random rnd = new Random();
+		int newX = rnd.nextInt(40);
+		int newY = rnd.nextInt(40);
+
+		if (newX + r.getX() > arenaXMax) {
+			r.setX(r.getX() - newX);
+		} else if (newX + r.getX() < 50) {
+			r.setX(r.getX() + newX);
+		} else {
+			r.setX(r.getX() + (newX * (-1 + ((newX % 2) * 2))));
+		}
+
+		if (newY + r.getY() > arenaYMax) {
+			r.setY(r.getY() - newY);
+		} else if (newY + r.getY() < 50) {
+			r.setY(r.getY() + newY);
+		} else {
+			r.setY(r.getY() + (newY * (-1 + ((newY % 2) * 2))));
+		}
+	}
+}
