a0f15eb5735ab68b0609b141b544a6c2bb16885d
Gael Fontaine <gael.fontaine@etu.unice.fr>
2018-01-07 10:28:59 +0100

    COrrection GraphismeCompliqué

--- plugins/src/main/java/plugins/GraphismeComplique.java
+++ plugins/src/main/java/plugins/GraphismeComplique.java
@@ -2,17 +2,20 @@ package plugins;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Robot;
+
+
+import annotation.Graphisme;
+import moteur.Robot;
 
 public class GraphismeComplique {
 	@Graphisme(nom = "GraphismeComplique")
 	public void draw(Robot r,Graphics g){
 		g.setColor(r.getColor());
-		g.fillRect(x, y, 40, 60);
-		g.fillRect(x-15, y-10, 15, 80);
-		g.fillRect(x+40, y-10, 15, 80);
+		g.fillRect(r.getX(), r.getY(), 40, 60);
+		g.fillRect(r.getX()-15, r.getY()-10, 15, 80);
+		g.fillRect(r.getX()+40, r.getY()-10, 15, 80);
 		g.setColor(Color.GRAY);
-		g.fillOval(x+5, y+15, 30, 30);
-		g.drawLine(x+20, y+20, x+20, y-10);
+		g.fillOval(r.getX()+5, r.getY()+15, 30, 30);
+		g.drawLine(r.getX()+20, r.getY()+20, r.getX()+20, r.getY()-10);
 	}
 }
