946fdb071c8d5307f163744bcc6774fdf49382a4
CHEN Datao <chendatao1126@gmail.com>
2018-01-08 16:26:38 +0100

    Ajout de la class AttaqueDistance

---
+++ plugins/src/main/java/pluginsAttaque/AttaqueDistance.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+import annotation.Attaque;
+import moteur.Projectile;
+import moteur.Robot;
+
+public class AttaqueDistance {
+	private static final int degatMini = 1;
+	private static final int degatMaxi = 2;
+}
